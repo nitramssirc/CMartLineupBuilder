4a11219ccb3d6218d4e74a7d28b46770d8d15c62
nitramssirc <nitramssirc@yahoo.com>
2023-10-15 15:36:32 -0400

    Vegas Line Checkpoint

---
+++ Domain/VegasLines/VegasLine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.VegasLines
+{
+    public class VegasLine
+    {
+        public Team Team { get; set; }
+
+        public Team Opponent { get; set; }
+
+        public bool IsHomeTeam { get; set; }
+
+        public decimal GameTotal { get; set; }
+
+        public decimal TeamPoints { get; set; }
+
+        public decimal Spread { get; set; }
+
+    }
+}
